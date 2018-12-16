4d1b72ba064a4eaa8f30cc5d353c24363f70e9de
leebrian <brianalee@gmail.com>
2018-12-16 09:32:41 -0500

    adding another type that shows up at top of results

--- remove-amazon-sponsored.user.js
+++ remove-amazon-sponsored.user.js
@@ -1,15 +1,25 @@
 // ==UserScript==
 // @name     Remove Amazon Sponsored
-// @version  .1
+// @version  .2
 // @grant    none
 // @include      *://www.amazon.com/*
 // ==/UserScript==
-//alert('hello')
+
+//these are the ones interspersed in results
 var ads = document.querySelectorAll(".AdHolder")
 for (var i=0, max=ads.length;i<max;i++){
   ads[i].style.display='none';
 }
-//alert('goodbye, deleted ' + i)
-if (i>0){
-	console.log('deleted ' + i);
+
+//this is the one up at the top of search results
+var more_ads = document.querySelectorAll(".slot__ad")
+for (var i=0, max=more_ads.length;i<max;i++){
+  more_ads[i].style.display='none';
 }
+
+var blocked_ads = ads.length+more_ads.length;
+
+if (blocked_ads>0){
+	console.log('deleted ' + blocked_ads);
+}
+
