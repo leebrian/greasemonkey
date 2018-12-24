962ea3637649b9c9be0030191eb0767acba91422
leebrian <brianalee@gmail.com>
2018-12-24 14:23:15 -0500

    adding check for refreshed elements so ads are removed clicking next and previous, etc etc

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Various handy scripts that I want to make sure to keep track of across machines.
 
 So far, just blocking Amazon's sponsored listings. Those are so annoying.
 
+Thanks <https://gist.github.com/BrockA> for your [waitforkeyelements gist](https://gist.github.com/BrockA/2625891)
--- remove-amazon-sponsored.user.js
+++ remove-amazon-sponsored.user.js
@@ -1,25 +1,37 @@
 // ==UserScript==
 // @name     Remove Amazon Sponsored
-// @version  .2
-// @grant    none
-// @include      *://www.amazon.com/*
+// @version  .3
+// @include   *://www.amazon.com/*
+// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant    GM_addStyle
+// thanks https://gist.github.com/BrockA
 // ==/UserScript==
 
-//these are the ones interspersed in results
-var ads = document.querySelectorAll(".AdHolder")
-for (var i=0, max=ads.length;i<max;i++){
-  ads[i].style.display='none';
+waitForKeyElements(".thirdPartySponsorLink, .AdHolder, .slot__ad", removeAds);
+
+function removeAds(jNode) {
+  var message = "Greasemonkey script to remove amazon sponsored ads ran";
+
+  //these are the ones interspersed in results
+  var ads = document.querySelectorAll(".AdHolder");
+  for (var i = 0, max = ads.length; i < max; i++) {
+    ads[i].style.display = "none";
+  }
+  message += "; deleted (" + max + ") inline ads";
+
+  //this is the one up at the top of search results
+  var ads = document.querySelectorAll(".slot__ad");
+  for (var i = 0, max = ads.length; i < max; i++) {
+    ads[i].style.display = "none";
+  }
+  message += "; deleted (" + max + ") slot ads";
+
+  //these are at the bottom
+  var ads = document.querySelectorAll(".thirdPartySponsorLink");
+  for (var i = 0, max = ads.length; i < max; i++) {
+    ads[i].style.display = "none";
+  }
+  message += "; deleted (" + max + ") bottom sponsor ads";
+  console.log(message);
 }
-
-//this is the one up at the top of search results
-var more_ads = document.querySelectorAll(".slot__ad")
-for (var i=0, max=more_ads.length;i<max;i++){
-  more_ads[i].style.display='none';
-}
-
-var blocked_ads = ads.length+more_ads.length;
-
-if (blocked_ads>0){
-	console.log('deleted ' + blocked_ads);
-}
-
